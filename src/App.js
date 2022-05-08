f763440a5f9c0e93ecd06530e17c857079728198
Aryan Shah <05aryanshah@gmail.com>
2022-05-09 02:50:42 +0530

    Complete Working Project

--- src/App.js
+++ src/App.js
@@ -1,39 +1,100 @@
-import logo from './logo.png';
+import {
+  Link,
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import { useState, useEffect } from 'react'
+import { ethers } from "ethers"
+import DecentratwitterAbi from './contractsData/decentratwitter.json'
+import DecentratwitterAddress from './contractsData/decentratwitter-address.json'
+import { Spinner, Navbar, Nav, Button, Container } from 'react-bootstrap'
+import logo from './logo.png'
+import Home from './Home.js'
+import Profile from './Profile.js'
 import './App.css';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [account, setAccount] = useState(null);
+  const [contract, setContract] = useState({});
+
+  const web3Handler = async () => {
+    let accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    setAccount(accounts[0]);
+
+    // Setup event listeners for metamask
+    window.ethereum.on('chainChanged', () => {
+      window.location.reload();
+    })
+    window.ethereum.on('accountsChanged', async () => {
+      setLoading(true);
+      web3Handler();
+    })
+    // Get provider from Metamask
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    // Get signer
+    const signer = provider.getSigner();
+    loadContract(signer);
+  }
+  const loadContract = async (signer) => {
+    // Get deployed copy of Decentratwitter contract
+    const contract = new ethers.Contract(DecentratwitterAddress.address, DecentratwitterAbi.abi, signer);
+    setContract(contract);
+    setLoading(false);
+  }
   return (
-    <div>
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a
-          className="navbar-brand col-sm-3 col-md-2 ms-3"
-          href="https://github.com/0xAryanShah"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aryan Shah Web3 Twitter
-        </a>
-      </nav>
-      <div className="container-fluid mt-5">
-        <div className="row">
-          <main role="main" className="col-lg-12 d-flex text-center">
-            <div className="content mx-auto mt-5">
-              <a
-                href="https://github.com/0xAryanShah"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={logo} className="App-logo" alt="logo" />
-              </a>
-              <h1 className="mt-5">Welcome to Web3 Twitter</h1>
-              <p>
-                Edit <code>src/frontend/components/App.js</code> and save to reload.
-              </p>              
+    <BrowserRouter>
+      <div className="App">
+        <>
+          <Navbar expand="lg" bg="primary" variant="dark">
+            <Container>
+              <Navbar.Brand href="https://github.com/0xAryanShah">
+                <img src={logo} width="40" height="40" className="" alt="" />
+                &nbsp; Web3 Twitter
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link as={Link} to="/">Home</Nav.Link>
+                  <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
+                </Nav>
+                <Nav>
+                  {account ? (
+                    <Nav.Link
+                      href={`https://etherscan.io/address/${account}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="button nav-button btn-sm mx-4">
+                      <Button variant="outline-dark">
+                        {account.slice(0, 5) + '...' + account.slice(38, 42)}
+                      </Button>
+
+                    </Nav.Link>
+                  ) : (
+                    <Button onClick={web3Handler} variant="outline-dark">Connect Wallet</Button>
+                  )}
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </>
+        <div>
+          {loading ? (
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '80vh' }}>
+              <Spinner animation="border" style={{ display: 'flex' }} />
+              <p className='mx-3 my-0'>Awaiting Metamask Connection...</p>
             </div>
-          </main>
+          ) : (
+            <Routes>
+              <Route path="/" element={<Home contract={contract} account = {account} />} />
+              <Route path="/profile" element={<Profile contract={contract} />} />
+            </Routes>
+          )}
         </div>
       </div>
-    </div>
+    </BrowserRouter>
+
   );
 }
 
